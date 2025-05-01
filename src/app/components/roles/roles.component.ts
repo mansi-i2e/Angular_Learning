90d5b80122e0b2f8976f3d6087de7e0458b75e28
Mansi Gehlot <mansi.gehlot@i2econsulting.com>
2025-05-01 21:39:12 +0530

    data, property and event binding & directives

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
@@ -16,4 +17,19 @@ export class RolesComponent {
   isActive: boolean = true;
 
   currentDate: Date = new Date();
+
+  inputType: string = "radio";
+
+  inputType1: string = "checkbox"
+
+  selectedState: string = '';
+
+  showWelcomeAlert() {
+    alert("Welcome to Angular 18 Tutorial")
+  }
+
+  showMessage(message: string) {
+    alert(message)
+  }
+
 }
