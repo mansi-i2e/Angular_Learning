790b70e32731f0e63ceb31a41cf837c7e16b400d
Mansi Gehlot <mansi.gehlot@i2econsulting.com>
2025-05-02 14:05:51 +0530

    API integration, struc directives, interface & service

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,35 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { APIResponseModel, IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
   // variable declaration
-  firstName: string = "My Angular Project";
-  angularVersion = "Version 19";
+  // firstName: string = "My Angular Project";
+  // angularVersion = "Version 19";
 
-  version: number = 18;
+  // version: number = 18;
 
-  isActive: boolean = true;
+  // isActive: boolean = true;
 
-  currentDate: Date = new Date();
+  // currentDate: Date = new Date();
 
-  inputType: string = "radio";
+  // inputType: string = "radio";
 
-  inputType1: string = "checkbox"
+  // inputType1: string = "checkbox"
 
-  selectedState: string = '';
+  // selectedState: string = '';
 
-  showWelcomeAlert() {
-    alert("Welcome to Angular 18 Tutorial")
+  // showWelcomeAlert() {
+  //   alert("Welcome to Angular 18 Tutorial")
+  // }
+
+  // showMessage(message: string) {
+  //   alert(message)
+  // }
+
+  roleList: IRole[] = [];
+  http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.getAllRoles()
   }
 
-  showMessage(message: string) {
-    alert(message)
+  getAllRoles() {
+    this.http.get<APIResponseModel>("/api/api/ClientStrive/GetAllRoles").subscribe((res: APIResponseModel) => {
+      console.log("API Response:", res);
+      this.roleList = res.data;
+    });
   }
+
 
 }
